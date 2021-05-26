e072864db171e3db3b532a785f6a8bee2a558066
adavid2021 <sleek12o12@gmail.com>
2021-05-25 21:42:10 -0400

    Changed project submission html and js.

--- public/js/project_submission.js
+++ public/js/project_submission.js
@@ -1,46 +1,45 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const error_message = urlParams.get("error_message");
-const project_id = urlParams.get("p_id");
-const input = JSON.parse(urlParams.get("input")); // converting back into json
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+// const error_message = urlParams.get("error_message");
+// const project_id = urlParams.get("p_id");
+// const input = JSON.parse(urlParams.get("input")); // converting back into json
 
 
+// if (project_id) {
+//     $('#project_id').val(project_id);
+// }
 
-if (project_id) {
-    $('#project_id').val(project_id);
-}
-
-if (error_message) {
-
-    if (error_message.includes('name')) {
-        $('#name').addClass('is-invalid text-danger');
-    }
-    if (error_message.includes('creator_name')) {
-        $('#creator_name').addClass('is-invalid text-danger');
-    }
-    if (error_message.includes('creator_email')) {
-        $('#creator_email').addClass('is-invalid text-danger');
-    }
-    if (error_message.includes('shooting_date')) {
-        $('#shooting_date').addClass('is-invalid text-danger');
-    }
-
-    if (error_message.includes('synopsis')) {
-        $('#synopsis').addClass('is-invalid text-danger');
-    }
-    if (error_message.includes('Role_des')) {
-        $('#Role_des').addClass('is-invalid text-danger');
-    }
-
-    let errList = error_message.substring(22,).split(',');
-    let output = ""
-
-    for (const err of errList) {
-        output += err.split(':')[1].substring(1,) + ", ";
-    }
-
-    $('#error_message').text(output.substring(0, output.length - 2));
-}
+// if (error_message) {
+//
+//     if (error_message.includes('name')) {
+//         $('#name').addClass('is-invalid text-danger');
+//     }
+//     if (error_message.includes('creator_name')) {
+//         $('#creator_name').addClass('is-invalid text-danger');
+//     }
+//     if (error_message.includes('creator_email')) {
+//         $('#creator_email').addClass('is-invalid text-danger');
+//     }
+//     if (error_message.includes('shooting_date')) {
+//         $('#shooting_date').addClass('is-invalid text-danger');
+//     }
+//
+//     if (error_message.includes('synopsis')) {
+//         $('#synopsis').addClass('is-invalid text-danger');
+//     }
+//     if (error_message.includes('Role_des')) {
+//         $('#Role_des').addClass('is-invalid text-danger');
+//     }
+//
+//     let errList = error_message.substring(22,).split(',');
+//     let output = ""
+//
+//     for (const err of errList) {
+//         output += err.split(':')[1].substring(1,) + ", ";
+//     }
+//
+//     $('#error_message').text(output.substring(0, output.length - 2));
+// }
 
 $('form').on('submit', function () {
     let errorMessage = null
@@ -57,6 +56,14 @@ $('form').on('submit', function () {
         return false;
     }
 
+    if ($('#name,textarea').val().length < 3) {
+        errorMessage = "The film name must be at least 3 characters long."
+        $('#name').addClass('is-invalid text-danger');
+        $('#error_message').text(errorMessage);
+
+        return false;
+    }
+
     if (!$('#creator_name,textarea').val()) {
         errorMessage = "Creator Name cannot be empty."
         $('#creator_name').addClass('is-invalid text-danger');
@@ -64,6 +71,15 @@ $('form').on('submit', function () {
 
         return false;
     }
+
+    if ($('#creator_name,textarea').val().length < 3) {
+        errorMessage = "The creator's name must be at least 3 characters long."
+        $('#creator_name').addClass('is-invalid text-danger');
+        $('#error_message').text(errorMessage);
+
+        return false;
+    }
+
     if (!$('#creator_email,textarea').val()) {
         errorMessage = "Creator Email cannot be empty."
         $('#creator_email').addClass('is-invalid text-danger');
@@ -81,7 +97,6 @@ $('form').on('submit', function () {
 
         return false;
     }
-    
 
     if (!$('#shooting_date,textarea').val()) {
         errorMessage = "Shooting Date cannot be empty."
@@ -90,6 +105,7 @@ $('form').on('submit', function () {
 
         return false;
     }
+
     if (!$('#synopsis,textarea').val()) {
         errorMessage = "Add some synopsis about your project."
         $('#synopsis').addClass('is-invalid text-danger');
@@ -97,8 +113,25 @@ $('form').on('submit', function () {
 
         return false;
     }
+
+    if ($('#synopsis,textarea').val().length < 3) {
+        errorMessage = "The synopsis must be at least 3 characters long."
+        $('#synopsis').addClass('is-invalid text-danger');
+        $('#error_message').text(errorMessage);
+
+        return false;
+    }
+
     if (!$('#Role_des,textarea').val()) {
         errorMessage = "Please provide some role descriptions of your project."
+        $('#Role_des').addClass('is-invalid text-danger');
+        $('#error_message').text(errorMessage);
+
+        return false;
+    }
+
+    if ($('#Role_des,textarea').val().length < 3) {
+        errorMessage = "The role description must be at least 3 characters long."
         $('#Role_des').addClass('is-invalid text-danger');
         $('#error_message').text(errorMessage);
 
