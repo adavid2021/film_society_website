4f6f03ca84d826e77c7531d4a84eeb90de10e9d7
adavid2021 <sleek12o12@gmail.com>
2021-05-28 22:18:12 -0400

    Split projects in index.html and index.js.

--- public/js/index.js
+++ public/js/index.js
@@ -1,62 +1,120 @@
 function showList(projects) {
     // console.log("projects: " + projects);
 
-    $('#project_list').empty();
+    $('#left_list').empty();
+    $('#right_list').empty();
+
+    if(projects.length === 0){
+    //    <h3><i>No projects match the search specifications.</i></h3>
+        $('#filters').append("<h3 style='text-align: center'><i>No projects match the search specifications.</i></h3>");
+    }
 
     for (let i = 0; i < projects.length; i++) {
-        $('#project_list').append("<li class='list-group-item'></li>");
+        if (i % 2 === 0) {
+            $('#left_list').append("<li class='list-group-item'></li>");
+        } else {
+            $('#right_list').append("<li class='list-group-item'></li>");
+        }
     }
 
     // added ObjectId to each object, now accessible on client side
-    $('#project_list li')
+    $('#left_list li')
         .attr("value", function (idx) {
-            return projects[idx]._id;
+            return projects[idx * 2]._id;
         })
         .append("<div class='row'></div>");
 
-    $('#project_list .row').addClass(function (idx) {
-        if (idx % 2 === 0) {
-            return 'even_row';
-        } else {
-            return 'odd_row';
-        }
-    });
+    $('#right_list li')
+        .attr("value", function (idx) {
+            return projects[(idx * 2) + 1]._id;
+        })
+        .append("<div class='row'></div>");
 
-    $('#project_list .row')
+    // $('#left_list .row').addClass(function (idx) {
+    //     if (idx % 2 === 0) {
+    //         return 'even_row';
+    //     } else {
+    //         return 'odd_row';
+    //     }
+    // });
+
+    $('#left_list .row')
         .append('<div class="card text-white" style="background-color: var(--cadet)">' +
             '  <div class="card-body">' +
-            '    <h5 class="card-title title"></h5>' +
+            '    <h5 class="card-title title_left"></h5>' +
             '    <div class="row justify-content-between"></div>' +
-            '       <div class="btn-group mybuttons" role="group">' +
+            '       <div class="btn-group mybuttons_left" role="group">' +
             '    </div>' +
             '  </div>' +
             '</div>')
 
-    $('.title').append(function (idx) {
-        return `<p>${projects[idx].title}</p>`
+    $('#right_list .row')
+        .append('<div class="card text-white" style="background-color: var(--cadet)">' +
+            '  <div class="card-body">' +
+            '    <h5 class="card-title title_right"></h5>' +
+            '    <div class="row justify-content-between"></div>' +
+            '       <div class="btn-group mybuttons_right" role="group">' +
+            '    </div>' +
+            '  </div>' +
+            '</div>')
+
+    $('.title_left').append(function (idx) {
+        return `<p>${projects[idx * 2].title}</p>`
+    });
+
+    $('.title_right').append(function (idx) {
+        return `<p>${projects[(idx * 2) + 1].title}</p>`
     });
 
     // note that when passing projects to the detail page we use the title as the parameter and replace spaces with "~"
-    $('.mybuttons').append(function (idx) {
+    $('.mybuttons_left').append(function (idx) {
         let button_html = `<p><b>Available Roles: &nbsp &nbsp</b></p>`;
-        const list = projects[idx].role_list.split(" ");
+        const list = projects[idx * 2].role_list.split(" ");
         list.forEach(role => {
             if (role) {
-                button_html += `<button class="btn btn-outline-dark" style="background-color: white; color: var(--burnt-sienna); border-color: var(--burnt-sienna); border-width: 2px">${role}</button>`
+                button_html += `<div class="card card-header" style="padding: 5px; background-color: white; color: var(--burnt-sienna); border-color: var(--burnt-sienna); border-width: 2px">${role}</div>`
             }
         });
         return button_html
     })
         .after(function (idx) {
-            return `<a href="/project_detail?project_id=${projects[idx]._id}" class="btn btn-outline-light interestbutton mt-4" style="background-color: var(--burnt-sienna)">I am interested!</a>`
+            return `<a href="/project_detail?project_id=${projects[idx * 2]._id}" class="btn btn-outline-light interestbutton mt-4" style="background-color: var(--burnt-sienna)">I am interested!</a>`
         })
         .after(function (idx) {
-            return `<p class="date"><b>Filming Date:</b> ${projects[idx].information[0].film_date}</p>`
+            return `<p class="date"><b>Filming Date:</b> ${projects[idx * 2].information[0].film_date}</p>`
         })
         .after(function (idx) {
-            return `<p class="synopsis"><b>Synopsis:</b> ${projects[idx].information[0].synopsis}</p>`
+            return `<div class="synopsis" style="margin-bottom: 20px"> ${projects[idx * 2].information[0].synopsis}</div>`
+        })
+        .after(function(){
+            return `<div><b>Synopsis:</b></div>`
+        });
+
+    //                button_html += `<button class="btn btn-outline-dark" style="background-color: white; color: var(--burnt-sienna); border-color: var(--burnt-sienna); border-width: 2px">${role}</button>`
+    $('.mybuttons_right').append(function (idx) {
+        let button_html = `<p><b>Available Roles: &nbsp &nbsp</b></p>`;
+        const list = projects[(idx * 2) + 1].role_list.split(" ");
+        list.forEach(role => {
+            if (role) {
+                button_html += `<div class="card card-header" style="padding: 5px; background-color: white; color: var(--burnt-sienna); border-color: var(--burnt-sienna); border-width: 2px">${role}</div>`
+            }
+        });
+        return button_html
+    })
+        .after(function (idx) {
+            return `<a href="/project_detail?project_id=${projects[(idx * 2) + 1]._id}" class="btn btn-outline-light interestbutton mt-4" style="background-color: var(--burnt-sienna)">I am interested!</a>`
+        })
+        .after(function (idx) {
+            return `<p class="date"><b>Filming Date:</b> ${projects[(idx * 2) + 1].information[0].film_date}</p>`
+        })
+        .after(function (idx) {
+            return `<div class="synopsis" style="margin-bottom: 20px"> ${projects[(idx * 2) + 1].information[0].synopsis}</div>`
+        })
+        .after(function(){
+            return `<div><b>Synopsis:</b></div>`
         });
 }
+
 
 //when entire page has been loaded execute this function
 $(document).ready(function () {
